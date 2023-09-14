99c1644a82197503fe98a73e83f2b8d87286c0f7
Jonatan melo <jonatanmelo058@gmail.com>
2023-09-14 20:41:13 -0300

    add class data types

--- 4-JAVASCRIPT/class-01-data-types/script.js
+++ 4-JAVASCRIPT/class-01-data-types/script.js
@@ -1,1 +1,30 @@
-console.log("hello world")
+
+
+let student ="Jonatan";
+let age="23";
+let weight=78.5;
+
+console.log( student);
+console.log(typeof 23);
+console.log(age);
+console.log( weight);
+
+let number1 =2;
+let number2 =5;
+
+console.log(2 + 5);
+
+let istudentincvt = false;
+
+console.log(istudentincvt);
+console.log(typeof istudentincvt);
+
+console.log(10 == 10);
+console.log(10 == 20);
+console.log(10 > 20);
+console.log(10 < 20);
+console.log(10 != 20);
+
+let yourname = prompt("Digite seu nome");
+
+console.log(yourname);
