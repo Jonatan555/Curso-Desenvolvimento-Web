864e41526057aaad1875dd08fb877fcae0cf5d1e
Jonatan melo <jonatanmelo058@gmail.com>
2023-09-27 20:33:40 -0300

    add  3-date.js

--- 4-JAVASCRIPT/class-04-data-manipulation/3-date.js
+++ 4-JAVASCRIPT/class-04-data-manipulation/3-date.js
@@ -1,5 +1,10 @@
 let dateNow = new Date();
 let date1 = new Date("2023-12-1 12:03:24");
+let date2 = new Date("2023-12-3 18:03:24")
+
+console.log((date2 - date1) /1000 / 60 / 60 / 24);
+console.log(0.25 * 24);
+
 
 console.log(dateNow);
 console.log(date1);
@@ -23,3 +28,10 @@ let month = String(dateNow.getMonth()).padStart(2, "0");
 let year = dateNow.getFullYear();
 
 console.log(`${day}/${month}/${year}`);
+
+console.log(dateNow.toLocaleDateString("pt-BR",{ 
+day: "2-digit",
+month: "long",
+year: "numeric"
+}));
+
