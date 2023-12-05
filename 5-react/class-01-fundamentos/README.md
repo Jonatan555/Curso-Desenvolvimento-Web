e8c92a750e7ef01ef2928eb41b9729403fd6f1f4
Jonatan melo <jonatanmelo058@gmail.com>
2023-12-05 20:25:04 -0300

    add class 01 fundamentls examples

---
+++ 5-react/class-01-fundamentos/README.md
@@ -0,0 +1,8 @@
+# Class 01 fundamentals
+
+## Topics
+
+ Introduction React Library
+ SPA & SSR
+ Installation
+ Syntex
---
+++ 5-react/class-01-fundamentos/src/App.tsx
@@ -0,0 +1,15 @@
+ import imageReact from "./assets/react.svg";
+ 
+ // named export
+  export function App() {
+    // fragment
+  return (
+    <>
+    <h1> Class 01 Fundamentals </h1>
+    <p> Syntax Jsx = JS + XML/HTML</p>
+    <img src="vite.svg" alt="" />
+    <img src={imageReact} alt="" />
+    </>
+  )
+  
+ }
