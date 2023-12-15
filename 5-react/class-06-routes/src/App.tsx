8c480f4506e2edefb0e5905ce7b68f15997e4ef8
Jonatan melo <jonatanmelo058@gmail.com>
2023-12-14 21:22:56 -0300

    add class routes

---
+++ 5-react/class-06-routes/src/App.tsx
@@ -0,0 +1,17 @@
+ import { Outlet } from "react-router-dom";
+import { Header } from "./components/Header";
+ 
+ export function App() {
+  return (
+    <>
+   <Header/>
+   <main>
+    <Outlet/>
+   </main>
+   
+   <footer>
+    @Reprograma Jucás
+   </footer>
+    </>
+  );
+ }
