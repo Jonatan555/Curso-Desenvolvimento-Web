fd5b282570b9746a08a9397dcdc03af3bce14932
jonatan de melo vieira <jonatanmelo058@gmail.com>
2024-03-13 20:46:33 -0300

    add route params examples

--- class-02-routes/src/routes/index.ts
+++ class-02-routes/src/routes/index.ts
@@ -1,15 +1,34 @@
 import { Router } from "express";
 
-export  const router= Router();
+export const router = Router();
 
 router.route("/").get((_req, res) => {
-    res.send({ name: "Jonatan"});
+  res.send({ name: "Jonatan" });
 });
 
- router.get("/pokemon", (_req, res) => {
-   res.send ([
-    {id: 1, name: "bulbasaur"},
-    {id: 150, name: "pikachu"},
-    {id: 6, name: "charizard"},
- ]);
- });
+router.get("/pokemon", (_req, res) => {
+  res.send([
+    { id: 1, name: "bulbasaur" },
+    { id: 150, name: "pikachu" },
+    { id: 6, name: "charizard" },
+  ]);
+});
+
+// route params
+router.get("/user/:id/:name", (req, res) => {
+  const { id, name } = req.params;
+  res.send({ id, name });
+});
+
+// query params
+router.get("/user", (req, res) => {
+  const { id, name, age } = req.query;
+  res.send({ id, name, age });
+});
+
+//  body params
+router.post("/user", (req, res) => {
+  const { id, name, age } = req.body;
+  console.log({ id, name, age });
+  res.send({ satus: "user create!" });
+});
--- class-02-routes/src/server.ts
+++ class-02-routes/src/server.ts
@@ -7,5 +7,7 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}...`);
 });
+//  middleware
+app.use(express.json());
 
 app.use(router);
