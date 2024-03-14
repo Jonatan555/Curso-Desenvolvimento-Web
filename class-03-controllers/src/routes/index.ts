0315b9ef114b58cb73692276dada497ab7eb6224
jonatan de melo vieira <jonatanmelo058@gmail.com>
2024-03-14 20:52:30 -0300

    add class-03-controllers

---
+++ class-03-controllers/src/routes/index.ts
@@ -0,0 +1,10 @@
+ import { Router } from "express";
+import { userControllers } from "../controllers/userControllers";
+
+export const router = Router(); 
+
+ router.post("/user", userControllers.create);
+ router.get("/user/:id", userControllers.read);
+
+
+
