3469c5d6199f8c4a7a165513fe71e1b355384a09
jonatan de melo vieira <jonatanmelo058@gmail.com>
2024-03-15 20:19:18 -0300

    add controllers update and, delete

--- class-03-controllers/src/controllers/userControllers.ts
+++ class-03-controllers/src/controllers/userControllers.ts
@@ -9,10 +9,30 @@ export const userControllers = {
             return;
         }
         
-        res.json ({ status: `user not created!`});
+        res.status(400).json ({ status: `user not created!`
+    });
     },
     read(req: Request, res: Response) {
         const { id } = req.params;
-        res.json({ user: id });
+        res.status(201).json({ user: id });
+       },
+
+       update(req: Request, res: Response) {
+        const { id } = req.params;
+        const { name, age } = req.body;
+
+        if (id && name && age) {
+            console.log("updatesd", { id, name, age});
+            res.status(200).json({status: `user ${id} updated!`});
+            return;
+        }
+
+        res.json({status: "user not updated!"});
+       },
+
+       delete(req: Request, res: Response) {
+        const { id } = req.params;
+        res.status(200).json({status: `user ${id} deleted`
+    });
        },
  };
