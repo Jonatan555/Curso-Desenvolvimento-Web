f6ac0938082964448f40e6381507c658be80ef92
jonatan de melo vieira <jonatanmelo058@gmail.com>
2024-03-22 21:16:42 -0300

    add create uuid, password hash and read controller

--- class-04-databases/src/controllers/index.ts
+++ class-04-databases/src/controllers/index.ts
@@ -21,25 +21,31 @@ export const userControllers = {
   },
 
   async read(req: Request, res: Response, next: NextFunction) {
-    const { id } = req.params;
-    const { password } = req.body;
-    const db = await sqliteConnection();
+    try {
+      const { id } = req.params;
+      const { password } = req.body;
+      const db = await sqliteConnection();
 
-    if (!password) {
-      throw res.status(400).json({ message: "please confirm your password!" });
+      if (!password) {
+        throw res
+          .status(400)
+          .json({ message: "please confirm your password!" });
+      }
+
+      const user = await db.get("SELECT * FROM users WHERE id = ?", [id]);
+      if (!user) throw res.status(400).json({ message: "user not found!" });
+
+      const passwordCheck = await compare(password, user.password);
+
+      if (!passwordCheck) {
+        throw res.status(401).json({ message: "invalid password!" });
+      }
+
+      const { name, email } = user;
+      return res.status(200).json({ name, email });
+    } catch (error) {
+      return next(error);
     }
-
-    const user = await db.get("SELECT * FROM users WHERE id = ?", [id]);
-    if (!user) throw res.status(400).json({ message: "user not found!" });
-
-    const passwordCheck = await compare(password, user.password);
-
-    if (passwordCheck) {
-      throw res.status(401).json({ message: "invalid password!" });
-    }
-
-    const {name, email } = user;
-    return res.status(200).json({ user });
   },
 
   update(req: Request, res: Response) {
