29c361467d421c852387d64120ce9f9c2276d07e
jonatan de melo vieira <jonatanmelo058@gmail.com>
2024-04-08 19:36:09 -0300

    add database connection

--- class-05-authentication/src/server.ts
+++ class-05-authentication/src/server.ts
@@ -3,6 +3,8 @@ import "dotenv/config";
 import { router } from "./@types";
 import { pageNotFound } from "./errors/pageNotFound";
 import { appErrors } from "./errors/appErrors";
+import { sqliteConnection } from "./databases/sqlite3";
+import { runMigrations } from "./databases/sqlite3/migrations";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,3 +18,9 @@ app.use(appErrors);
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
+
+sqliteConnection()
+  .then(() => console.log("Database is connected..."))
+  .catch((error) => console.error("Database isn't connected -", error));
+
+  runMigrations();
